95664e089f323a1d9b069dd15ccf40a7ad1ac14f
Laedson <laedson2535@gmail.com>
2025-06-21 15:56:13 -0300

    Adding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "./components/navigation/navigation.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/main/my-car/my-car.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-car',
+  imports: [],
+  templateUrl: './my-car.component.html',
+  styleUrl: './my-car.component.css'
+})
+export class MyCarComponent {
+
+}
